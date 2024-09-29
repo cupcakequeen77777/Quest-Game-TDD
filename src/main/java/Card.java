a023e312a8c98161b8ef159856f33b0e4fd21539
cupcakequeen77777 <leeanneking@cmail.carleton.ca>
2024-09-29 19:44:00 -0400

    RESP_02_test_02

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,24 +1,40 @@
 
 
 public class Card {
-        int cardValue;
-        String type;
+    int cardValue;
+    String type;
 
-        public Card() {
-            new Card(0, "");
-        }
-
-        public Card(int value, String t) {
-            cardValue = value;
-            type = t;
-        }
-
-        public int GetFoeCardValue() {
-            return cardValue;
-        }
-
-        public String GetCardType() {
-            return type;
-        }
-
+    public Card() {
+        new Card(0, "");
     }
+
+    public Card(int value, String t) {
+        cardValue = value;
+        type = t;
+    }
+
+    public int GetFoeCardValue() {
+        return cardValue;
+    }
+
+    public String GetCardType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return type + cardValue;
+    }
+
+    // TODO: Displaying the hand of a player means listing foes first in increasing order, then weapons, also in increasing order, with swords before horses.
+    public int compare(Card card) {
+        if (type.equals("F") && !card.type.equals("F")) {
+            return -1;
+        } else if (card.type.equals("F") && !type.equals("F")) {
+            return 1;
+        }
+        return Integer.compare(cardValue, card.cardValue);
+    }
+
+
+}
