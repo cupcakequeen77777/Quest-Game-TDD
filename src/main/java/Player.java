68f966fca27f34b509147584732b7841c595a867
cupcakequeen77777 <leeanneking@cmail.carleton.ca>
2024-09-29 19:56:14 -0400

    REFAC - Added Shields to Player

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -4,10 +4,12 @@ public class Player {
     int playerNumber;
     ArrayList<Card> hand;
     final int MAX_CARDS = 12;
+    int shields;
 
     public Player(int playerNumber) {
         this.playerNumber = playerNumber;
         hand = new ArrayList<>(MAX_CARDS);
+        shields = 0;
     }
 
 
